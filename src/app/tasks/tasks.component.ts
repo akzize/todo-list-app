1c223ee1bed5446e9688c0772889a89baad6fc2c
akzize <akziz2zakaria@gmail.com>
2024-12-20 18:10:23 +0100

    refactor the code and the add edit and make tasks completed funcs

--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -6,39 +6,47 @@ import { Task, TaskService } from '../task.service';
   templateUrl: './tasks.component.html',
   styleUrls: ['./tasks.component.css'],
 })
-export class TasksComponent implements OnInit{
+export class TasksComponent implements OnInit {
   // tasks to be shown
-  tasks: Task[] = []
+  tasks: Task[] = [];
+  editMode: boolean = false;
+  targetTask: number = 0;
 
   constructor(private taskService: TaskService) {
-    this.tasks = this.taskService.getTasks()
+    this.tasks = this.taskService.getTasks();
   }
 
   ngOnInit(): void {
     // now here we should subscribe to the observer to keep up with updates
-    this.taskService.tasks$.subscribe(tasks => {
+    this.taskService.tasks$.subscribe((tasks) => {
       this.tasks = tasks;
-    })
+    });
   }
 
   getTasks() {
-    this.tasks = [...this.taskService.getTasks()]
+    this.tasks = [...this.taskService.getTasks()];
   }
 
   // edit task
   editTask(title: string) {
-    if(!title.trim()){
-      return
+    if (!title.trim()) {
+      return;
     }
-
-
   }
   deleteTask(id: number) {
     this.taskService.deleteTask(id);
 
     // update the tasks list
-    this.getTasks()
-    console.log("delete ", this.taskService.getTasks());
+    this.getTasks();
+    console.log('delete ', this.taskService.getTasks());
+  }
 
+  toggleEdit(id: number) {
+    this.editMode = !this.editMode;
+    this.targetTask = id;
+  }
+
+  toggleComplete(task: Task) {
+    task.completed = !task.completed;
   }
 }
